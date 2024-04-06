6c782572d8328384b364b6fdf1e5fb9abacfe94f
KukeiAMG <159035810+KukeiAMG@users.noreply.github.com>
2024-04-06 17:13:48 +0500

    write switch/case

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("\t\t------Switch_Case------\n");
+
+        Scanner scanner = new Scanner(System.in);
+        int age = 0;
+        switch(age){
+            case 0:
+                System.out.println("age = 0\n");
+                break;
+            case 2:
+                System.out.println("age = 2\n");
+        }
+
+        System.out.println("\t\t------Arrays------");
     }
-    int a = 12;
-    int b = 13;
-
-    int c  = a + b;
-
 }
